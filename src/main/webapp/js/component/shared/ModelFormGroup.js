5b7e6db41b001d08c33a0953013aa434a009351b
Jordan Walker <jiwalker@usgs.gov>
2011-09-29 17:18:33 -0500

    Dataset not allows add and edit

--- src/main/webapp/js/component/shared/ModelFormGroup.js
+++ src/main/webapp/js/component/shared/ModelFormGroup.js
@@ -8,7 +8,38 @@ Ext.define("ncETL.panel.ModelFormGroup", {
 		var _items = [];
 		
 		_store.on('load', function(store, records, successful, operation, options) {
-			store.each(function(record) {
+			this.reload();
+		}, this);
+		
+        _items.push(new Ext.button.Button({
+            text : "Add " + _store.model.getName(), // TODO is this kosher Sibley?
+            handler : function(record) {
+				var form = new ncETL.form.Model({
+					model : this.store.model.getName(),
+					defaults : {
+						anchor : '100%'
+					}
+				});
+				this.store.add(record);
+                this.reload(this.store);
+			},
+            scope : this
+        }));
+        
+		config = Ext.apply({
+		items : _items	
+		}, config);
+		ncETL.panel.ModelFormGroup.superclass.constructor.call(this, config);
+	},
+	saveRecords : function() {
+		this.items.each(function(item){
+            if (item.saveRecord) {
+                item.saveRecord();
+            }
+		}, this);
+	},
+    reload : function() {
+        this.store.each(function(record) {
 				var form = new ncETL.form.Model({
 					model : record.self.getName(),
 					defaults : {
@@ -18,16 +49,5 @@ Ext.define("ncETL.panel.ModelFormGroup", {
 				form.loadRecord(record);
 				this.add(form);
 			}, this);
-		}, this);
-		
-		config = Ext.apply({
-		items : _items	
-		}, config);
-		ncETL.panel.ModelFormGroup.superclass.constructor.call(this, config);
-	},
-	saveRecords : function() {
-		this.items.each(function(item){
-			item.saveRecord();
-		}, this);
-	}
+    }
 });
