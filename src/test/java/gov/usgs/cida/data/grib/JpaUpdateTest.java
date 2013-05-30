d9caca9fee3fc72cdf26260e5ea80aff150b92a0
R Hayes <rhayes@usgs.gov>
2013-05-30 18:45:54 -0500

    IMPLEMENTING - issue GDP-598: RFC Real Time Archive Builder https://internal.cida.usgs.gov/jira/browse/GDP-598
    JPA update configuration.

---
+++ src/test/java/gov/usgs/cida/data/grib/JpaUpdateTest.java
@@ -0,0 +1,89 @@
+package gov.usgs.cida.data.grib;
+
+import static org.junit.Assert.*;
+import gov.usgs.cida.ncetl.jpa.ArchiveConfig;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import org.hibernate.ejb.HibernateEntityManagerFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+public class JpaUpdateTest {
+
+	private ConfigurableApplicationContext context;
+	
+	@Before
+	public void init() {
+		context = new ClassPathXmlApplicationContext("jpa-update-context.xml");
+	}
+	
+	@Test
+	public void testStart() throws Exception {
+		
+		 try {
+		 
+			 System.out.printf("loaded\n");
+			 
+			 context.start();
+			 
+			 Thread.sleep(5*1000);
+			 
+			 assertTrue("survived", true);
+		 } finally {
+			 context.close();
+		 }
+	}
+	
+	@Test
+	public void updateConnected() throws Exception {
+		EntityManagerFactory emf = context.getBean("emf", EntityManagerFactory.class);
+		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction t = em.getTransaction();
+		t.begin();
+		
+		TypedQuery<ArchiveConfig> q = em.createQuery("select s from ArchiveConfig s ", ArchiveConfig.class);
+		
+		q.setMaxResults(1);
+		
+		ArchiveConfig arc = q.getSingleResult();
+		
+		arc.addHistory("steam heat");
+		
+		t.commit();
+	}
+
+	@Test
+	public void updateDetached() throws Exception {
+		EntityManagerFactory emf = context.getBean("emf", EntityManagerFactory.class);
+		
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction t = em.getTransaction();
+		
+		t.begin();
+		
+		TypedQuery<ArchiveConfig> q = em.createQuery("select s from ArchiveConfig s ", ArchiveConfig.class);
+		q.setMaxResults(1);
+		
+		ArchiveConfig arc = q.getSingleResult();
+		
+		em.detach(arc);
+		t.commit();
+		
+		
+		
+		t.begin();
+		arc = em.merge(arc);
+		arc.addHistory("detached heat");
+		t.commit();
+	}
+
+}
